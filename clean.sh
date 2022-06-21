97c15c93e997a7558a14864823d09964af30b67d
dean.sundquist@gmail.com <dean.sundquist@gmail.com>
2022-06-21 09:26:22 -0500

    Forgot a file

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Cleanup Certs
-rm -f ~/go/bin/server.key ~/go/bin/server.cert ~/go/bin/server.csr ~/go/bin/client.key ~/go/bin/client.crt ~/go/bin/client.csr 
+rm -f ~/go/bin/server.key ~/go/bin/server.crt ~/go/bin/server.csr ~/go/bin/client.key ~/go/bin/client.crt ~/go/bin/client.csr 
