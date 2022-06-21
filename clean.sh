837e5fc1cbcb64b05ca8f8a7e610c03280f6f33e
dean.sundquist@gmail.com <dean.sundquist@gmail.com>
2022-06-21 09:29:09 -0500

    Adding some echo statements

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 # Cleanup Certs
+
+echo "Attempting to remove the following: ~/go/bin/server.key ~/go/bin/server.crt ~/go/bin/server.csr ~/go/bin/client.key ~/go/bin/client.crt ~/go/bin/client.csr  "
 rm -f ~/go/bin/server.key ~/go/bin/server.crt ~/go/bin/server.csr ~/go/bin/client.key ~/go/bin/client.crt ~/go/bin/client.csr 
--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ mkdir -p ~/go/bin/public
 cp ./public/index.html ~/go/bin/public
 go install .;
 # Generate SSH Cert
+echo "Generating Server Certificates in ~/go/bin: "
 openssl req  -new  -newkey rsa:2048  -nodes  -keyout ~/go/bin/server.key  -out ~/go/bin/server.csr
 openssl  x509  -req  -days 365  -in ~/go/bin/server.csr  -signkey ~/go/bin/server.key  -out ~/go/bin/server.crt
 # Generate mTLS Cert 
+echo "Generating Client Certificates in ~/go/bin: "
 openssl req  -new  -newkey rsa:2048  -nodes  -keyout ~/go/bin/client.key  -out ~/go/bin/client.csr
 openssl  x509  -req  -days 365  -in ~/go/bin/server.csr  -signkey ~/go/bin/client.key  -out ~/go/bin/client.crt
 
